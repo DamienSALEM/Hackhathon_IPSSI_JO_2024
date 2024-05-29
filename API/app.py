3d94459fb5b15edd7fdc54348cfea0bcf5e06b5c
habakar <abakarhayar@gmail.com>
2024-05-29 17:44:52 +0200

    ajout de filtre bd

--- API/app.py
+++ API/app.py
@@ -1,62 +1,54 @@
-
-import os
-from datetime import datetime
-from typing import List
-
-import MySQLdb
-import sshtunnel
-from dotenv import load_dotenv
-from fastapi import FastAPI
-from sqlalchemy import MetaData, Table, create_engine
-from sqlalchemy.orm import sessionmaker
-
-# Charger les variables d'environnement depuis le fichier .env
-load_dotenv()
-
-sshtunnel.SSH_TIMEOUT = 10
-sshtunnel.TUNNEL_TIMEOUT = 10
-
-
-db_user=os.getenv('DB_USER')
-db_password=os.getenv('DB_PASSWORD')
-host='127.0.0.1'
-port="3306"
-db_name=os.getenv('DB_NAME')
-
-
-
-try:
-    with sshtunnel.SSHTunnelForwarder(
-        (os.getenv('SSH_HOST')),
-        ssh_username=os.getenv('SSH_USERNAME'), ssh_password=os.getenv('SSH_PASSWORD'),
-        remote_bind_address=(os.getenv('DB_HOST'), 3306)
-    ) as tunnel:
-        database_url = f"mysql://{db_user}:{db_password}@127.0.0.1:{tunnel.local_bind_port}/{db_name}"
-        engine  = create_engine(database_url)
-        session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-        with engine.connect() as db:
-        
-          metadata = MetaData()
-          metadata.reflect(bind=engine, only=['olympic_hosts'])
-  
-          olympic_hosts = Table('olympic_hosts', metadata, autoload_with=engine)
-          Session = sessionmaker(bind=engine)
-          session = Session()
-
-    print("Connexion réussie à la base de données MySQL via le tunnel SSH.")
-except Exception as e:
-    print("Erreur:", e)
-
+# app.py
+from fastapi import FastAPI, HTTPException 
+from connect_bd import get_metadata
 
 app = FastAPI()
 
+## TABLE olympic_hosts
 
-# Todo : filter by tag 
+@app.get("/olympic_hosts/count")
+async def get_olympic_hosts_count():
+    try:
+        metadata = get_metadata()
+        olympic_hosts = metadata["olympic_hosts"]
+        session = metadata["session"]
+        tunnel = metadata["tunnel"]
+        
+        count_olympic_hosts = session.query(olympic_hosts).count()
+        session.close()
+        tunnel.stop()
+        return {"count_olympic_hosts": count_olympic_hosts}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-@app.get("/olympic_hosts/")
-async def read_olympic_hosts( ):
-    olympic_hosts = Table('olympic_hosts', metadata, autoload_with=engine)
-    columns = olympic_hosts.columns.keys()
-    print(columns)
-    return columns
+@app.get("/olympic_hosts/filter_by_country")
+async def filter_olympic_hosts_by_country(country_name: str):
+    try:
+        metadata = get_metadata()
+        olympic_hosts = metadata["olympic_hosts"]
+        session = metadata["session"]
+        tunnel = metadata["tunnel"]
+        
+        query = session.query(olympic_hosts).filter(olympic_hosts.columns.game_location == country_name)
+        result = [row._asdict() for row in query]
+        session.close()
+        tunnel.stop()
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))    
+    
+## TABLE olympic_athletes    
+@app.get("/olympic_athletes/count")
+async def get_olympic_athletes_count():
+    try:
+        metadata = get_metadata()
+        olympic_athletes = metadata["olympic_athletes"]
+        session = metadata["session"]
+        tunnel = metadata["tunnel"]
+        
+        olympic_athletes_count = session.query(olympic_athletes).count()
+        session.close()
+        tunnel.stop()
+        return {"olympic_athletes_count": olympic_athletes_count}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
