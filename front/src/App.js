5ec894a8825e53f39fcfdf4f3fb7ec26576af0b3
madi niakate accenture <madi.niakate@accenture.com>
2024-05-29 15:12:18 +0200

    add PredictionPage branchee

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Error404 from './pages/Error404';
-import Home from './pages/Home';
-import Navbar from './components/Header';
+import React from 'react'
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom'
+import Navbar from './components/Header'
+import Error404 from './pages/Error404'
+import Home from './pages/Home'
+import PredictionPage from './pages/predictionPage'
 
 const App = () => {
   return (
@@ -12,9 +18,10 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="pageIntrouvable" element={<Error404 />} />
         <Route path="*" element={<Navigate to="pageIntrouvable" replace />} />
+        <Route path="predictionPage" element={<PredictionPage />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ front/src/pages/PredictionPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function PredictionPage() {
+  return <div>predictionPage</div>
+}
