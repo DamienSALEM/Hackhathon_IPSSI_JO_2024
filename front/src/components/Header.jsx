4ea05106d5b04ad24c3bae25e829564484fa597a
Saïd Bohui <bohuisaid@hotmail.fr>
2024-05-29 12:27:52 +0200

    update home

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -13,21 +13,12 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-
-// interface Props {
-// 	/**
-// 	 * Injected by the documentation to work in an iframe.
-// 	 * You won't need it on your project.
-// 	 */
-// 	window?: () => Window;
-// }
-
+import Logo from "../assets/logo.svg";
 
 const drawerWidth = 240;
-const navItems = ['Home', 'About', 'Contact'];
+const navItems = ['Home', 'Fun facts', 'predictor'];
 
 const Navbar = () => {
-//   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
@@ -36,9 +27,9 @@ const Navbar = () => {
 
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
+      <Box sx={{ my: 2 }}>
+        <img src={Logo} alt="Logo" style={{ height: 60 }} />
+      </Box>
       <Divider />
       <List>
         {navItems.map((item) => (
@@ -52,12 +43,10 @@ const Navbar = () => {
     </Box>
   );
 
-//   const container = window !== undefined ? () => window().document.body : undefined;
-
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar component="nav">
+      <AppBar component="nav" sx={{ backgroundColor: '#416082' }}>
         <Toolbar>
           <IconButton
             color="inherit"
@@ -68,13 +57,9 @@ const Navbar = () => {
           >
             <MenuIcon />
           </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            MUI
-          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+            <img src={Logo} alt="Logo" style={{ height: 60 }} />
+          </Box>
           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
             {navItems.map((item) => (
               <Button key={item} sx={{ color: '#fff' }}>
@@ -86,7 +71,6 @@ const Navbar = () => {
       </AppBar>
       <nav>
         <Drawer
-        //   container={container}
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
@@ -105,5 +89,4 @@ const Navbar = () => {
   );
 }
 
-
-export default Navbar
+export default Navbar;
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import { Box, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, tableCellClasses } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { styled } from "@mui/material/styles";
+import bg from "../assets/imgJO.webp";
+// import bannerParis from "../assets/bannerParis.webp";
+// import bannerParis from "../assets/banner.webp";
+import bannerParis from "../assets/newBanner.webp";
+import DataRow from "../components/DataRow";
+const axios = require('axios');
+
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: "#17222F",
+  // backgroundColor: theme.palette.common.black,
+  color: theme.palette.common.white,
+},
+[`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+        backgroundColor: "#070F04",
+        color: "white"
+},
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText("#9C27B0"),
+  backgroundColor: "#9C27B0",
+  "&:hover": {
+    backgroundColor: "#7B1EA2",
+  },
+}));
+
+
+  const [datas, setDatas] = useState([]);
+  
+  let arrayTestData = [
+    { Date: 'Janvier', Pays: 'France', Medaille: 'Bronze' },
+    { Date: 'Mars', Pays: 'Russie', Medaille: 'Argent' },
+    { Date: 'Février', Pays: 'USA', Medaille: 'Or' }
+  ];
+
+  const getDatas = async () => {
+    try {
+      let datas = await axios.get(`http://localhost:8000`);
+      console.log("datas: ", datas.data);
+      setDatas(arrayTestData);
+      return datas.data;
+    } catch (error) {
+      console.log("error: ", error);
+      return error;
+    }
+  };
+
+  useEffect(() => {
+    // getDatas();
+    setDatas(arrayTestData);
+  }, []);
+
+  return (
+    <Box sx={{ height: "100vh", overflow: "hidden" }}>
+      <Box
+        sx={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${bannerParis})`,
+          backgroundRepeat: "no-repeat",
+          // backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Box>
+          
+        </Box>
+        <TableContainer sx={{ width: "60%", maxHeight: "40%", overflowY: "scroll" }}>
+          <Table stickyHeader aria-label="collapsible table">
+            <TableHead>
+              <TableRow>
+                <TableCell />
+                <TableCell>Citation</TableCell>
+                <TableCell />
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {datas.map((data, key) => (
+                <DataRow key={key} data={data} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
